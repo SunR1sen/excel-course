c48c008532d19b5dc8fd6419f4b9729341f8850a
SunR1sen <sunr1sen.dev@gmail.com>
2020-08-12 16:38:30 +0300

    Framework start (#4)
    * Finish base framework functional

---
+++ src/core/DomListener.js
@@ -0,0 +1,42 @@
+import {capitalize} from '@core/utils';
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`);
+    }
+    this.$root = $root;
+    this.listeners = listeners;
+  }
+
+  initDOMListeners() {
+    // console.log(this.listeners, this.$root);
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(
+          `Method ${method} is not
+          implemented in ${this.name || ''} Component`
+        );
+      }
+      this[method] = this[method].bind(this);
+      // Аналог addEventListener
+      this.$root.on(listener, this[method]);
+    })
+  }
+
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    })
+  }
+}
+
+// input => onInput
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
+
